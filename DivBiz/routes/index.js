43659d7c1fc1095613acd39a41294675121556ff
smithrm941 <smithrm941@gmail.com>
2017-05-24 15:48:25 -0700

    WIP individual business pages are rendering working on getting all business info on them

--- DivBiz/routes/index.js
+++ DivBiz/routes/index.js
@@ -9,9 +9,34 @@ router.get('/', function(req, res, next) {
 
 router.get('/businesses', function(req, res) {
   res.render('businesses', {
-    title: 'Businesses',
-    firstBusinessName: sampleData[0].name
+    title: 'MBE/DBE Businesses in Maryland',
+    businessArray: sampleData
   });
 });
+
+//example from bookstore:
+// router.get('/book/:id', (req, res) => {
+//   const id = req.params.id
+//   db.viewBook(id)
+//   .then(books => {
+//     res.render('bookdetail', { books: books})
+//   } )
+// })
+
+router.get('/business-detail/:certification_number', function(req, res) {
+  console.log("made it into certification number get");
+  var {certification_number} = req.params
+  console.log(certification_number, "certification number")
+  var businessObjectData
+  for (var i = 0; i < sampleData.length; i++) {
+    if (sampleData[i].certification_number === certification_number) {
+      businessObjectData = sampleData[i]
+    }
+  }
+  res.render('business-detail', {
+    businessObject: businessObjectData
+  })
+
+})
 
 module.exports = router;
